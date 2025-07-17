676f8807486b2b2913c05cba644cdce19466e2e2
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2025-07-17 14:27:50 +0200

    Step 2 of tutorial
    https://shiljopaulson.medium.com/lets-refactor-c-code-cce70446a093

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,16 +6,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        ArrayList myNumbers = new ArrayList();
-        myNumbers.Add(1);
-        myNumbers.Add(2);
-        myNumbers.Add(3);
-        myNumbers.Add(4);
-        myNumbers.Add(5);
-
-        for (int i = 0; i < myNumbers.Count; i++)
+        //I see the problem here lol. The numbers are not scoped.
+        List<int> numbers = new List<int>{1, 2, 3, 4, 5};
+        
+        foreach (int number in numbers)
         {
-            int number = (int)myNumbers[i];
             if (number % 2 == 0)
             {
                 Console.WriteLine("Number is even: " + number);
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8ecc34a906ea29df028464d27ac7b1371d9c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
